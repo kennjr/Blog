f812516b5ac1fbaa632c0006594d49e83e932d0e
kennjr <ken.juniour@student.moringaschool.com>
2022-03-13 08:37:27 +0300

    First commit after creating most of the files need ed for the project

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello Wrld"
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
