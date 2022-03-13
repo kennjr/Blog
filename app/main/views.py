1d363c9e777b39528bf2e4a3a2f2f04fbd924086
kennjr <ken.juniour@student.moringaschool.com>
2022-03-14 01:06:36 +0300

    Added code in the index for getting the username for the user if they're logged in

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,21 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, session
 from flask import request as req
 from werkzeug.utils import redirect
-
+from flask_login import current_user
 from app.main import main
 
 
 @main.route('/')
 def index():
     """Render the index template"""
+    username = None
+    try:
+        if current_user:
+            username = current_user.username
+    except AttributeError:
+        print('An attr error occurred')
 
-    return render_template('index.html', title="Th index")
+    return render_template('index.html', username=username)
 
 
 @main.route('/blogs/create')
@@ -23,4 +29,15 @@ def new_blog():
         print(f'The blog\'s title is {title}, the description is {description} and the text is {blog_txt}')
         return redirect(url_for('main.index'))
     return render_template('forms/new_blog.html')
+
+
+@main.route("/blogs/<int:blog_id>")
+def view_blog(blog_id):
+
+    return render_template('blog_view.html', blog_id=blog_id)
+
+
+@main.route("/blogs/<int:blog_id>/comments")
+def view_blog_comments(blog_id):
+    return render_template('comments/comments_list.html', blog_id=blog_id)
 
