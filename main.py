9d807fa137651b2ed862d9d1f4d37b842daabecb
kennjr <ken.juniour@student.moringaschool.com>
2022-03-14 16:47:11 +0300

    Heroku commit after deleting the migration files

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def make_shell_ctx():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
-    # manager.run()
+    # app.run()
+    manager.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
